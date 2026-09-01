010809b561a4a7d4f56901adc69e0a0afc254af0
VulpineFriend87 <vulpinefriend87@gmail.com>
2026-09-01 19:45:35 +0200

    feat: log through ComponentLogger, adding Velocity support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,20 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly(libs.paper)
+    compileOnly(libs.adventure.api)
+    compileOnly(libs.adventure.minimessage)
+    compileOnly(libs.adventure.serializer.legacy)
+    compileOnly(libs.adventure.serializer.plain)
+    compileOnly(libs.adventure.logger)
 
-    testImplementation(libs.paper)
+    testImplementation(libs.adventure.api)
+    testImplementation(libs.adventure.minimessage)
+    testImplementation(libs.adventure.serializer.legacy)
+    testImplementation(libs.adventure.serializer.plain)
+    testImplementation(libs.adventure.logger)
     testImplementation(libs.junit)
     testRuntimeOnly(libs.junit.launcher)
 }
